ede7d26f5a673c6fa8fe52aaa78a63cd6fdb9251
Anna <zharova1992@gmail.com>
2022-07-11 23:35:06 +0400

    задача 36!

--- Program.cs
+++ Program.cs
@@ -42,3 +42,52 @@ ShowArray(ArrayRand);
 
 int count = CountNum(ArrayRand);
 Console.WriteLine("Количество четных чисел в массиве: " + count); */
+
+
+
+/* Решение задачи №36
+
+int[] Array (int size, int minRan, int maxRan)
+{
+
+    int [] newArray = new int[size];
+
+    for (int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minRan, maxRan);
+
+    return newArray;    
+}
+
+void ShowArray (int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int SummNumNeg (int [] array)
+{   
+    int summ = 0;
+    for(int i=0; i<array.Length; i++)
+        if (i % 2 != 0)
+            summ+=array[i];
+return summ;
+}
+
+Console.WriteLine("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение числа для массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение числа для массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] ArrayRand = Array(size, min, max);
+Console.Write("Массив, заполненный случайными числами: ");
+ShowArray(ArrayRand);
+
+int NegativEl = SummNumNeg(ArrayRand);
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: " + NegativEl); */
+
+
+
