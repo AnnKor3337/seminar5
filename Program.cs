c0816f177c3ad3c59bee91d3e6114e5c3a9b1fcd
Anna <zharova1992@gmail.com>
2022-07-13 23:53:23 +0400

    Задача 38!

--- Program.cs
+++ Program.cs
@@ -91,3 +91,62 @@ Console.WriteLine("Сумма элементов, стоящих на нечёт
 
 
 
+/* Решение задачи №38
+
+double[] Array (int size, double minRan, double maxRan)
+{
+    double[] newArray = new double[size];
+
+    for (int i = 0; i < size; i++)
+        newArray[i] = new Random().NextDouble();
+
+    return newArray;    
+}
+
+void ShowArray (double[] array)
+{
+    for(int i=0; i<array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+double MaxMin (double [] array)
+{   
+    double max, min, diff;
+    diff = 0;
+    int i=0;
+    max = array[i];
+    min = array[i];   
+
+    for (; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i]; 
+        }
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+    }
+        diff = max - min;
+    Console.WriteLine("Максимальный элемент массива: " + max);
+    Console.WriteLine("Минимальный элемент массива: " + min);
+    return diff;
+}
+ 
+Console.WriteLine("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение числа для массива: ");
+double min = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение числа для массива: ");
+double max = Convert.ToDouble(Console.ReadLine());
+
+double[] ArrayRand = Array(size, min, max);
+Console.Write("массив, заполненный случайными числами: ");
+ShowArray(ArrayRand);
+Console.WriteLine();
+
+double DiffMaxMin = MaxMin (ArrayRand);
+Console.WriteLine("Разница между максимальным и минимальным числами в массиве: " + DiffMaxMin); */
